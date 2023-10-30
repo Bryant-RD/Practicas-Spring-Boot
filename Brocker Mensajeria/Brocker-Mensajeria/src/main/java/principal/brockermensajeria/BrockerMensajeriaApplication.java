2c0138f8f8789bc6f5cf065f0f9fbe0b639f337d
IvanPaul1515 <ipmx0001@ce.pucmm.edu.do>
2023-10-29 22:49:39 -0400

    subiendo clases de servidor,

--- Brocker Mensajeria/Brocker-Mensajeria/src/main/java/principal/brockermensajeria/BrockerMensajeriaApplication.java
+++ Brocker Mensajeria/Brocker-Mensajeria/src/main/java/principal/brockermensajeria/BrockerMensajeriaApplication.java
@@ -8,10 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BrockerMensajeriaApplication {
 
     public static void main(String[] args) {
-        // Inicializa el servidor JMS embebido
+
         startEmbeddedActiveMQBroker();
 
-        // Inicia la aplicación Spring Boot
         SpringApplication.run(BrockerMensajeriaApplication.class, args);
         System.out.println("SERVIDOR ARRIBA");
     }
@@ -19,7 +18,7 @@ public class BrockerMensajeriaApplication {
     private static void startEmbeddedActiveMQBroker() {
         try {
             BrokerService broker = new BrokerService();
-            broker.addConnector("tcp://localhost:61616"); // Puerto en el que escuchará
+            broker.addConnector("tcp://localhost:61616");
             broker.start();
         } catch (Exception e) {
             e.printStackTrace();
