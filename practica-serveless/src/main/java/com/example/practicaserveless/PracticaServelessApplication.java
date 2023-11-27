748dc1ec2622cbee0cbd8fa6e7b97c8147f964dd
Bryant-RD <briant04.rt@gmail.com>
2023-11-27 14:59:04 -0400

    firt commit

---
+++ practica-serveless/src/main/java/com/example/practicaserveless/PracticaServelessApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaserveless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaServelessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaServelessApplication.class, args);
+    }
+
+}
---
+++ practica-serveless/src/test/java/com/example/practicaserveless/PracticaServelessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaserveless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaServelessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
