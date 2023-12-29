65dbdef0bd26f941d6f5be10afce5165b0b25fc6
Bryant-RD <briant04.rt@gmail.com>
2023-12-29 14:28:26 -0400

    test

---
+++ Proyecto final/pixel-picture-app/src/API/auth.js
@@ -0,0 +1,21 @@
+const tokenKey = 'JWT';
+
+export const setToken = token => {
+    localStorage.setItem(tokenKey, token);
+}
+
+export const getToken = () => localStorage.getItem(tokenKey);
+
+export const deleteToken = () => {
+    localStorage.removeItem(tokenKey)
+}
+
+const fetchWithAuthorization = async (url, options = {}) => {
+    const headers = {
+      ...options.headers,
+      Authorization: `Bearer ${getToken()}`,
+    };
+  
+    const response = await fetch(url, { ...options, headers });
+    return response;
+  };
